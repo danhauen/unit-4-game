0471bef839f76af46958c97bddd893a405d9c42d
Dan Hauenstein <danhauen@gmail.com>
2020-01-07 17:16:03 -0600

    touch up to css and still not resetting gem numbers after round is over

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
  // runs on page load ?????
  // window.onload = function() {
-$(document).ready(function () {  
+   
  // vars for win , loss , score 
  var winCount = 0;
  var lossCount = 0;
  var score = 0;
 
 // randNumber score to match range from 19-120
- var randNum = Math.floor(Math.random() * (120)) + 19;
+ var randNum = Math.floor(Math.random() * (102)) + 19;
 
 // crystal values created
  var gemNum = Math.floor(Math.random() * (12)) + 1;
@@ -24,60 +24,64 @@ $(document).ready(function () {
  randNumText.text(randNum);
  scoreText.text(score);
 
+
+    // reset when game is over
+    function reset() {
+        randNum = Math.floor(Math.random() * (102)) + 19;
+        gemNum = Math.floor(Math.random() * (12)) + 1;
+        winCountText.text(winCount);
+        lossCountText.text(lossCount);
+        randNumText.text(randNum);
+        score = 0;
+        scoreText.text(score);   
+    }
+
     // button to reset for restarting game and resetting values
     $("#reset-btn").on("click", function() {
-        randNum = Math.floor(Math.random() * (120)) + 19;
-        randNumText.text(randNum)
+        randNum = Math.floor(Math.random() * (102)) + 19;
         gemNum = Math.floor(Math.random() * (12)) + 1;
         winCount = 0;
         winCountText.text(winCount);
         lossCount = 0;
         lossCountText.text(lossCount);
+        randNumText.text(randNum);
         score = 0;
         scoreText.text(score);
-    });
+        });
+       
 
-    // reset when game is over
-    function reset() {
-        randNum = Math.floor(Math.random() * (120)) + 19;
-        gemNum = Math.floor(Math.random() * (12)) + 1;
-        winCountText.text(winCount);
-        lossCountText.text(lossCount);
-        randNumText.text(randNum)
-        score = 0;
-        scoreText.text(score);
-    }
+    $(document).ready(function() {
 
-    // establish gem numbers 1-12
-    $(".gem-btn").each(function(){
-        var gemNum = Math.floor(Math.random() * (12)) + 1;
-        $(this).attr("data-value", gemNum);    
-    }) 
-        
-    // using gem numbers from above to generate score
-    $(".gem-btn").on("click", function () {
-        score = score + parseInt($(this).attr("data-value"));
+    
+        // establish gem numbers 1-12
+        $(".gem-btn").each(function(){
+            var gemNum = Math.floor(Math.random() * (12)) + 1;
+            $(this).attr("gem-value", gemNum); 
+        }) 
+       
+        // using gem numbers from above to generate score
+        $(".gem-btn").on("click",function() {
+            score = score + parseInt($(this).attr("gem-value"));
 
+                // if your score is equal to score to match then win +1
+                if (score === randNum) {
+                    winCount++;
+                    // reset
+                    reset();
+                }
 
-        // if your score is equal to score to match then win +1
-        if (score === randNum) {
-            winCount++;
-            // reset
-            reset();
-        }
+                // if your score is greater than score to match then loss +1
+                else if (score > randNum) {
+                    lossCount++;
+                    // reset
+                    reset();
+                }
 
-        // if your score is greater than score to match then loss +1
-        else if (score > randNum) {
-                lossCount++;
-                // reset
-                reset();
-        }
-
-        else {
-                winCountText.text(winCount);
-                lossCountText.text(lossCount);
-                randNumText.text(randNum);
-                scoreText.text(score);
-            }
+                else {
+                    winCountText.text(winCount);
+                    lossCountText.text(lossCount);
+                    randNumText.text(randNum);
+                    scoreText.text(score);
+                }
         });
     });
