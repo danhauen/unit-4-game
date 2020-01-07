60a9ae1d8883f3bce84657c70b474da463420cbe
Dan Hauenstein <danhauen@gmail.com>
2020-01-07 14:23:44 -0600

    everything funtioning except gem reset number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
-// gets page ready
-$(document).ready(function () {
-     
+ // runs on page load ?????
+ // window.onload = function() {
+$(document).ready(function () {  
  // vars for win , loss , score 
  var winCount = 0;
  var lossCount = 0;
@@ -24,6 +24,18 @@ $(document).ready(function () {
  randNumText.text(randNum);
  scoreText.text(score);
 
+    // button to reset for restarting game and resetting values
+    $("#reset-btn").on("click", function() {
+        randNum = Math.floor(Math.random() * (120)) + 19;
+        randNumText.text(randNum)
+        gemNum = Math.floor(Math.random() * (12)) + 1;
+        winCount = 0;
+        winCountText.text(winCount);
+        lossCount = 0;
+        lossCountText.text(lossCount);
+        score = 0;
+        scoreText.text(score);
+    });
 
     // reset when game is over
     function reset() {
@@ -31,55 +43,41 @@ $(document).ready(function () {
         gemNum = Math.floor(Math.random() * (12)) + 1;
         winCountText.text(winCount);
         lossCountText.text(lossCount);
-        randNumText.text(randNum);
+        randNumText.text(randNum)
         score = 0;
         scoreText.text(score);
-      
     }
-                // not functioning
-    // button to reset for restarting game and resetting values
-    //$("#reset-btn").on("click", function() {
-       // randNum = Math.floor(Math.random() * (120)) + 19;
-       // gemNum = Math.floor(Math.random() * (12)) + 1;
-       // winCount = 0;
-       // lossCount = 0;
-       // score = 0;
-       // });
-       // console.log(randNum);
+
+    // establish gem numbers 1-12
+    $(".gem-btn").each(function(){
+        var gemNum = Math.floor(Math.random() * (12)) + 1;
+        $(this).attr("data-value", gemNum);    
+    }) 
+        
+    // using gem numbers from above to generate score
+    $(".gem-btn").on("click", function () {
+        score = score + parseInt($(this).attr("data-value"));
 
 
+        // if your score is equal to score to match then win +1
+        if (score === randNum) {
+            winCount++;
+            // reset
+            reset();
+        }
 
-        // gives each gem a number between 1-12
-        $(".gem-btn").each(function(){
-            var gemNum = Math.floor(Math.random() * (12)) + 1;
-            $(this).attr('data-value', gemNum);
-        }) 
-        console.log(gemNum);
+        // if your score is greater than score to match then loss +1
+        else if (score > randNum) {
+                lossCount++;
+                // reset
+                reset();
+        }
 
-        $(".gem-btn").on("click", function () {
-            score = score + parseInt($(this).attr('data-value'));
-            console.log($(this).attr('data-value'));
-
-
-                // if your score is equal to score to match then win +1
-                if (score === randNum) {
-                    winCount++;
-                    // reset
-                    reset();
-                }
-
-                // if your score is greater than score to match then loss +1
-                else if (score > randNum) {
-                    lossCount++;
-                    // reset
-                    reset();
-                }
-
-                else {
-                    winCountText.text(winCount);
-                    lossCountText.text(lossCount);
-                    randNumText.text(randNum);
-                    scoreText.text(score);
-                }
+        else {
+                winCountText.text(winCount);
+                lossCountText.text(lossCount);
+                randNumText.text(randNum);
+                scoreText.text(score);
+            }
         });
     });
