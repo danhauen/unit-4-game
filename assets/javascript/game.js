374b469d0cdf37c3225d937289114e1258de02dd
Dan Hauenstein <danhauen@gmail.com>
2020-02-11 10:18:44 -0600

    added jquery for no zoom

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,6 +22,34 @@
  randNumText.text(randNum);
  scoreText.text(score);
 
+//  function to prevent zoom on double tap on mobile devices
+ (function($) {
+    var count = 0;
+    $.fn.nodoubletapzoom = function() {
+        $(this).bind('touchstart', function preventZoom(e){
+            var t2 = e.timeStamp;
+            var t1 = $(this).data('lastTouch') || t2;
+            var dt = t2 - t1;
+            var fingers = e.originalEvent.touches.length;
+            $(this).data('lastTouch', t2);
+            if (!dt || dt > 500 || fingers > 1){
+                return; // not double-tap
+            }
+            e.preventDefault(); // double tap - prevent the zoom
+            // also synthesize click events we just swallowed up
+            $(e.target).trigger('click');
+        });
+    };
+    })(jQuery);
+
+    $(document).on('pageinit',"#status_page", function(){
+        $("body").nodoubletapzoom();
+        $(".content").on("click", "#but", function() {
+            var curr_val = parseInt($("#val").text());
+            $("#val").text(curr_val + 1);
+        });
+});
+
 
     // reset when game is over
     function reset() {
